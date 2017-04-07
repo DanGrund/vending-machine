3759cf398fb80e967b237f629baa3654d95710ef
DanGrund <hello@dangrund.com>
2017-04-07 14:10:12 -0600

    level 3 completed

--- tests/vendingMachineTest.js
+++ tests/vendingMachineTest.js
@@ -1,9 +1,8 @@
-require('babel-core/register')({
-  ignore: /node_modules\/(?!ProjectB)/
-});
+require('babel-core/register')({ignore: /node_modules\/(?!ProjectB)/});
 const assert = require('chai').assert
 const VendingMachine = require('../vendingMachine').default;
 const Person = require('../person').default
+const Treat = require('../treat').default
 
 describe('Vending Machine', () => {
 
@@ -14,6 +13,14 @@ describe('Vending Machine', () => {
     vendingMachine.reset();
     dan.reset()
   });
+
+  it('is instantiated with properties for status, credits, change, selection, & treats', () => {
+    assert.equal(vendingMachine.state.status, 'idle')
+    assert.equal(vendingMachine.state.credits, 0)
+    assert.equal(vendingMachine.state.change, 0)
+    assert.equal(vendingMachine.state.selection, null)
+    assert.deepEqual(vendingMachine.state.treats, [{ name: 'treat', price: 75 }])
+  })
 
   it('will accept currency', () => {
     assert.equal(vendingMachine.state.status, 'idle')
@@ -47,7 +54,34 @@ describe('Vending Machine', () => {
     vendingMachine.checkCredits()
     vendingMachine.dispense(dan)
     assert.equal(dan.state.credits, 425)
-
   })
 
 });
+
+describe('Treat', () => {
+  const treat = new Treat('marlboro', 500, 10)
+
+  it('is instantiated with properties for name, price, and quantity', () => {
+    assert.equal(treat.state.name, 'marlboro')
+    assert.equal(treat.state.price, 500)
+    assert.equal(treat.state.quantity, 10)
+  })
+})
+
+describe('Person', () => {
+  const dan = new Person()
+
+  afterEach(() => {
+    dan.reset()
+  });
+
+  it('is instantiated with a property for credit', () => {
+    assert.equal(dan.state.credits, 500)
+  })
+
+  it('can borrow money from mom',() => {
+    assert.equal(dan.state.credits, 500)
+    dan.begMomForMoney()
+    assert.equal(dan.state.credits, 1000)
+  })
+})
